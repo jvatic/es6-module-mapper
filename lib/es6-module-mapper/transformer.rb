227387a091915b396e2e5ca2b5c00737c1e917cf
Jesse Stuart <jesse@jessestuart.ca>
2015-03-10 18:16:57 -0400

    Handle different names for nodejs executable
    Signed-off-by: Jesse Stuart <jesse@jessestuart.ca>

--- lib/es6-module-mapper/transformer.rb
+++ lib/es6-module-mapper/transformer.rb
@@ -4,7 +4,8 @@ require 'yajl'
 module ES6ModuleMapper
   class Transformer
     VERSION = '1'
-    TRANSFORMER_CMD = 'node '+ File.join(File.dirname(__FILE__), 'transformer.js')
+    NODEJS_CMD = %w[node nodejs].map { |cmd| %x{which #{cmd}}.chomp }.find { |cmd| !cmd.empty? }
+    TRANSFORMER_CMD = "#{NODEJS_CMD} #{File.join(File.dirname(__FILE__), 'transformer.js')}"
 
     MODULES_GLOBAL_VAR_NAME = 'window.____modules____'
     MODULES_LOCAL_VAR_NAME = '__m__'
